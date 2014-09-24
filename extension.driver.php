4c2626cbd2dd4aaa8a23c37edc0a6f2c8b46443a
Brian Zerangue <brian.zerangue@gmail.com>
2014-09-24 08:13:14 -0500

    Update extension.driver.php
    updated order of jqueryui and initailise scripts

--- extension.driver.php
+++ extension.driver.php
@@ -17,8 +17,8 @@
 			$assets_path = '/extensions/jquery_date_picker/assets/';
 			
 			# load jQuery and autocomplete JS
-			$page->addScriptToHead(URL . $assets_path . 'jquery-ui-core-datepicker.js', 430);
-			$page->addScriptToHead(URL . $assets_path . 'initialise.js', 431);
+			$page->addScriptToHead(URL . $assets_path . 'jquery-ui-core-datepicker.js', 4430);
+			$page->addScriptToHead(URL . $assets_path . 'initialise.js', 4431);
 			
 			# load autocomplete styles
 			$page->addStylesheetToHead(URL . $assets_path . 'ui.core.css', 'screen', 110);
